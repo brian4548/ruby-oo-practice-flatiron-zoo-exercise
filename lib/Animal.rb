bc4f271ec834d6329ad9e7cf45d6f4ce44092515
brian4548 <brianr4548@gmail.com>
2020-11-30 17:18:08 -0600

    commit tree fd457e2c2e404eabe367d284fd09e99e4eaa87aa

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,24 @@
 class Animal
 
+    attr_reader :specie :nickname
+    attr_accessor :weight
+    
+    @@all = []
+    
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight 
+        @nickname = nickname
+        self.class.all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def zoo
+        Zoo.all.select do |zoo|
+            zoo == self
+    end
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,17 @@
 class Zoo
+    
+    attr_accessor :name :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
